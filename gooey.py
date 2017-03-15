548396bce0e84ca51cdd182ff067a7f4ea6d020f
carloscesar182 <carloscesar182@gmail.com>
2017-03-15 20:58:11 -0300

    comment´s translation

--- gooey.py
+++ gooey.py
@@ -1,44 +1,54 @@
+#  Import the library code
 from tkinter import *
 import pygame.mixer
 
+#  Event handlers that set the IntVar and play the appropriate sound
+def play_correct_sound():
+    num_good.set(num_good.get() + 1)
+    s_correct.play()
+
+def play_wrong_sound():
+    num_bad.set(num_bad.get() + 1)
+    s_wrong.play()
+
+#  Creates the Gui application window
+app = Tk()
+app.title("TVN Game Show")
+app.geometry('300x100+200+100')
+
+#  Initialize the sound system
 sounds = pygame.mixer
 sounds.init()
 
+#  Load the required sound effects
 s_correct = sounds.Sound("correct.wav")
 s_wrong = sounds.Sound("wrong.wav")
 
-number_correct = 0
-number_wrong = 0
+#  Create the IntVars to count the answers
+num_good = IntVar()
+num_good.set(0)
+num_bad = IntVar()
+num_bad.set(0)
 
+#  Creates a friendly message that tells what to do
+lab = Label(app, text = 'Where you are ready, click on the buttons.', height = 3)
+#  Be sure to pack your widgets
+lab.pack()
 
-def play_correct_sound():
-    global number_correct
-    number_correct += 1
-    s_correct.play()
+#  A label to count and connect to the relevant IntVar
+lab1 = Label(app, textvariable = num_good)
+lab1.pack(side = 'left')
 
+#  A label to count and connect to the relevant IntVar
+lab2 = Label(app, textvariable = num_bad)
+lab2.pack(side = 'right')
 
-def play_wrong_sound():
-    global number_wrong
-    number_wrong += 1
-    s_wrong.play()
-
-
-def prompt():
-    print(str(number_correct) + " were correctly answered.")
-    print(str(number_wrong) + " were answered incorrectly.")
-
-
-app = Tk()  # cria uma janela chamada "app"
-app.title("TVN Game Show")  # dá um nome para a janela
-app.geometry('300x100+200+100')  # configura a coordenada e o tamanho da janela
-
-b1 = Button(app, text = "Correct!", width = 10, command = play_correct_sound)  # adiciona o botao
-b1.pack(side = 'left', padx = 10, pady = 10)  # linka o botão com a janela
+#  A button connected with its relevant event handler
+b1 = Button(app, text = "Correct!", width = 10, command = play_correct_sound)  # add the button
+b1.pack(side = 'left', padx = 10, pady = 10)  # links the button and the window
 
 b2 = Button(app, text = "Wrong!", width = 10, command = play_wrong_sound)
 b2.pack(side = 'right', padx = 10, pady = 10)
 
-b3 = Button(app, text = "Quit", width = 10, command = prompt)
-b3.pack(side = 'bottom', padx = 10, pady = 10)
-
-app.mainloop()  # inicia o evento
+#  Starts the tkinter mainloop
+app.mainloop()
