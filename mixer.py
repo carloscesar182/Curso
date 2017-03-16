ebdea6607b434a7c901ec9448693172e5b26605e
carloscesar182 <carloscesar182@gmail.com>
2017-03-16 18:43:39 -0300

    mixer program 1st version - 2buttons

--- mixer.py
+++ mixer.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pygame.mixer
+from tkinter.messagebox import askokcancel
 
 app = Tk()
 app.title("Head First Mix")
@@ -16,9 +17,18 @@ def track_start():
 def track_stop():
     track.stop()
 
+
+# A function created to stop the song if the user click on the close button before stop the song
+def shutdown():
+    track.stop()
+    app.destroy()
+    """if askokcancel(title='Are you sure?', message='Do you really want to quit?'):  # A question message box
+        app.destroy()  # It will end de app and will stop the song"""
+
 track = mixer.Sound(sound_file)
 
 start_button = Button(app, command=track_start, text='Start').pack(side=LEFT)
 stop_button = Button(app, command=track_stop, text='Stop').pack(side=RIGHT)
 
+app.protocol("WM_DELETE_WINDOW", shutdown)  # An event protocol to execute a specific code instead the default one
 app.mainloop()
